94c46689a543ddaf60aeb9a139dd603967eabcb2
Ifechimenim Ikwukala-Mbas <mbas750@gmail.com>
2025-10-15 04:06:35 +0100

    add fixture to access page objects

---
+++ fixtures/pom.fixture.ts
@@ -0,0 +1,24 @@
+// Creating a Playwright fixture that initializes a Page Object Manager (PomManager) for each test.
+// This allows us to access the Page Objects(LoginPage, SecurePage, etc), through the manager.
+// The fixture also provides a valid user object for authentication tests.
+
+import { test as base } from '@playwright/test';
+import PomManager from '../pages/ManagePage';
+import validUser from '../test-data/validUser.json';
+
+type MyFixtures = {
+    pm: PomManager;
+    validUser: { username: string; password: string };
+};
+
+export const test = base.extend<MyFixtures>({
+    pm: async ({ page }, use) => {
+        const pm = new PomManager(page);
+        await use(pm);
+    },
+    validUser: async (_, use) => {
+        await use(validUser);
+    },
+});
+
+export { expect } from '@playwright/test';
